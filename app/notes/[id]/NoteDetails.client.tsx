088099a0cff189a7923af475f1e9c8fb28b2fdf7
Denys Tryshkin <denystryshkin@gmail.com>
2026-09-17 23:37:27 -0400

    add intercepted note preview modal

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -27,18 +27,20 @@ export default function NoteDetailsClient() {
   }
 
   return (
-    <div className={css.container}>
-      <div className={css.item}>
-        <div className={css.header}>
-          <h2>{note.title}</h2>
+    <main className={css.main}>
+      <div className={css.container}>
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>{note.title}</h2>
+          </div>
+
+          <p className={css.tag}>{note.tag}</p>
+
+          <p className={css.content}>{note.content}</p>
+
+          <p className={css.date}>{note.createdAt}</p>
         </div>
-
-        <p className={css.tag}>{note.tag}</p>
-
-        <p className={css.content}>{note.content}</p>
-
-        <p className={css.date}>{note.createdAt}</p>
       </div>
-    </div>
+    </main>
   );
 }
--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -19,7 +19,9 @@ const NoteList = ({ notes, onDelete }: NoteListProps) => {
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
 
-            <Link href={`/notes/${note.id}`}>View details</Link>
+            <Link href={`/notes/${note.id}`} className={css.link}>
+              View details
+            </Link>
 
             <button className={css.button} onClick={() => onDelete(note.id)}>
               Delete
